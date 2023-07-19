43e3c5f0caa7f748f525d042b088afd7be5c1b30
Hikmethan Can <hikmethann@gmail.com>
2023-07-19 09:59:05 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Article,Brand,Cta } from './components'
+
+const App = () => {
+  return (
+    <div>
+      <h1> GPT-3</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {defaults as Article} from './article/Article';
+export {defaults as Brand} from './brand/Brand';
+export {defaults as Cta} from './cta/Cta';
+export {defaults as Feature} from './feature/Feature';
+export {defaults as Navbar} from './navbar/Navbar';
